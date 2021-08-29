42e689d1d672468b0b4776897940ad2cccf2d867
githashem <PersonalHashem@gmail.com>
2021-08-29 19:44:21 +0430

    second commit

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'eshop_contact.apps.EshopContactConfig',
     'eshop_settings.apps.EshopSettingsConfig',
     'eshop_card.apps.EshopCardConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,13 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'cdn', 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'cdn', 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
