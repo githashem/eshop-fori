3ea354d98481b86468e987b55ef0035dc92d7bdc
githashem <PersonalHashem@gmail.com>
2021-08-29 21:28:42 +0430

    7

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,10 @@ from .models import Post
 class Blog(ListView):
     model = Post
     paginate_by = 3
+    queryset = Post.objects.all().filter(is_publish=True)
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    queryset = Post.objects.all().filter(is_publish=True)
